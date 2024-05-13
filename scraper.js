46a0eaa5247ec6dfd548c9e36d9dafd62a2d03cc
Nicholas Osorio <nicholas.osorio98@gmail.com>
2024-05-13 14:12:27 -0400

    Fixed issue with scraper.js file

--- scraper.js
+++ scraper.js
@@ -1,116 +1,107 @@
 // https://www.npmjs.com/package/nightmare
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Retry-After 
-
-// https://chatgpt.com/c/6795a669-f91c-41f0-a482-7c6b357a70c7 >> MIGHT BE SOLUTION
+// https://chatgpt.com/c/6795a669-f91c-41f0-a482-7c6b357a70c7 >> Chat logs with ChatGPT
 
 import Nightmare from 'nightmare';
-const nightmare = Nightmare({ waitTimeout: 25000 });
 
-// one function to search based on 5 different sites
+// One function to search based on 5 different sites.
 export async function scrapePrice(URL, value) {
     try {
-        //amazon link - works fine
+        const nightmare = Nightmare({ waitTimeout: 25000 });
+        //Amazon link - Works fine.
         if (URL == "https://www.amazon.com/Sobro-SOCTB300-Refrigerator-Bluetooth-Speakers/dp/B07S85JTZD") { 
             const priceString = await nightmare.goto(URL)
                 .wait('#corePrice_feature_div')
                 .evaluate(() => document.getElementById("corePrice_feature_div").firstElementChild.firstElementChild.firstElementChild.firstElementChild.innerHTML)
-                .end()
-                .then(console.log("ended"));
+                .end();
 
             const priceNumber = parseFloat(priceString.replace('$','').replace(',','')); 
 
             // If price reduced, send green. If price is the same, send gray. If price increases, send red. All must send the price + a color.
             if(priceNumber < value) {
-                return { Price: priceNumber, Color: "Green", Message: "On sale! :D"}; //on sale
+                return { Price: priceNumber, Color: "Green", Message: "On sale! :D"};
             }
             else if (priceNumber == value) {
-                return { Price: priceNumber, Color: "Gray", Message: "Same price."}; //remains unchanged
+                return { Price: priceNumber, Color: "Gray", Message: "Same price."};
             }
             else {
-                return { Price: priceNumber, Color: "Red", Message: "Price went up :("}; //price increase
+                return { Price: priceNumber, Color: "Red", Message: "Price went up :("};
             }  
         }
-        //wayfair link - can give 429 error code with not many requests, but code has proven to work before
+        //Wayfair link - Can give 429 error code with not many requests, but code has proven to work before.
         else if (URL == "https://www.wayfair.com/furniture/pdp/sobro-smart-coffee-table-sobr1001.html?piid%5B0%5D=26336002") { 
             const priceString = await nightmare.goto(URL)
                 .wait('.SFPrice')
                 .evaluate(() => document.querySelector('.SFPrice').firstElementChild.firstElementChild.innerHTML)
-                .end()
-                .then(console.log("ended"));
-        
+                .end();
+                    
             const priceNumber = parseFloat(priceString.replace('$','').replace(',',''));
 
             if(priceNumber < value) {
-                return { Price: priceNumber, Color: "Green", Message: "On sale! :D"}; //on sale
+                return { Price: priceNumber, Color: "Green", Message: "On sale! :D"};
             }
             else if (priceNumber == value) {
-                return { Price: priceNumber, Color: "Gray", Message: "Same price."}; //remains unchanged
+                return { Price: priceNumber, Color: "Gray", Message: "Same price."};
             }
             else {
-                return { Price: priceNumber, Color: "Red", Message: "Price went up :("}; //price increase
+                return { Price: priceNumber, Color: "Red", Message: "Price went up :("};
             }  
         }
-        //sobro link - works fine
+        //Sobro link - Works fine.
         else if (URL == "https://sobrodesign.com/products/sobro-coffee-table") { 
             const priceString = await nightmare.goto(URL)
                 .wait('.regular-price')
                 .evaluate(() => document.querySelector('.regular-price').innerHTML)
-                .end(console.log("ended"));
-                //.then(console.log("ended"));
-            
-            nightmare.end();
-            console.log(typeof(priceString)); //doesn't go here on second attempt
+                .end();
+
             const priceNumber = parseFloat(priceString.replace('$','').replace(',',''));
-            console.log("retrieved priceNumber");
 
             if (priceNumber < value) {
-                return { Price: priceNumber, Color: "Green", Message: "On sale! :D"}; //on sale
+                return { Price: priceNumber, Color: "Green", Message: "On sale! :D"};
             }
             else if (priceNumber == value) {
-                return { Price: priceNumber, Color: "Gray", Message: "Same price."}; //remains unchanged
+                return { Price: priceNumber, Color: "Gray", Message: "Same price."};
             }
             else {
-                return { Price: priceNumber, Color: "Red", Message: "Price went up :("}; //price increase
+                return { Price: priceNumber, Color: "Red", Message: "Price went up :("};
             } 
         }
-        //wellbots link - works fine
+        //wellbots link - Works fine.
         else if (URL == "https://www.wellbots.com/products/sobro-smart-coffee-table") {
             const priceString = await nightmare.goto(URL)
                 .wait('.money')
                 .evaluate(() => document.querySelector('.price').querySelector('.money').innerHTML)
-                .end()
-                .then(console.log("ended"));
+                .end();
         
             const priceNumber = parseFloat(priceString.replace('$','').replace(',',''));
 
             if(priceNumber < value) {
-                return { Price: priceNumber, Color: "Green", Message: "On sale! :D"}; //on sale
+                return { Price: priceNumber, Color: "Green", Message: "On sale! :D"};
             }
             else if (priceNumber == value) {
-                return { Price: priceNumber, Color: "Gray", Message: "Same price."}; //remains unchanged
+                return { Price: priceNumber, Color: "Gray", Message: "Same price."};
             }
             else {
-                return { Price: priceNumber, Color: "Red", Message: "Price went up :("}; //price increase
+                return { Price: priceNumber, Color: "Red", Message: "Price went up :("};
             }  
         }
-        //macys link - brings 403 error with .goto() method. code should work + abide by the robots.txt but for some reason still unable to scrape page
+        //Macy's link - Brings 403 error with .goto() method. Code should work + abide by the robots.txt, but for some reason still unable to scrape page.
         else if (URL == "https://www.macys.com/shop/featured/sobro%20coffee%20table/Pageindex/1?cm_kws_ls=sobro%20coffee%20table") {
             const priceString = await nightmare.goto(URL)
                 .wait(3000)
                 .evaluate(() => document.querySelector('.discount').lastChild.textContent)
-                .end()
-                .then(console.log("ended"));
-            
+                .end();
+
             const priceNumber = parseFloat(priceString.replace('$','').replace(',',''));
 
             if(priceNumber < value) {
-                return { Price: priceNumber, Color: "Green", Message: "On sale! :D"}; //on sale
+                return { Price: priceNumber, Color: "Green", Message: "On sale! :D"};
             }
             else if (priceNumber == value) {
-                return { Price: priceNumber, Color: "Gray", Message: "Same price."}; //remains unchanged
+                return { Price: priceNumber, Color: "Gray", Message: "Same price."};
             }
             else {
-                return { Price: priceNumber, Color: "Red", Message: "Price went up :("}; //price increase
+                return { Price: priceNumber, Color: "Red", Message: "Price went up :("};
             } 
         }
 
@@ -122,6 +113,3 @@ export async function scrapePrice(URL, value) {
         console.log("An error occured executing the code.");
     }
 }
-
-// const _scrapePrice = { scrapePrice };
-// export { _scrapePrice as scrapePrice };
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 //npm run dev - terminal command that starts server with nodemon so changes to server code immediately apply on live server
+//npx kill-port 3000 - command that kills port 3000 (can replace 3000 with whatever)
+//https://stackoverflow.com/questions/15718649/how-to-publish-a-website-made-by-node-js-to-github-pages - possible server hosts
 
 import express from 'express';
 import cors from 'cors'
@@ -12,9 +14,7 @@ app.use(cors());
 // Define route to scrape prices
 app.get('/scrape', async (req, res) => {
     try {
-        console.log("route accessed")
-        const { URL, VALUE } = req.query; //ask about req.query
-        console.log("const declared")
+        const { URL, VALUE } = req.query;
         const info = await scrapePrice(URL, VALUE);
         res.status(200).json(info);
     } catch (error) {
@@ -24,7 +24,7 @@ app.get('/scrape', async (req, res) => {
 });
 
 // Start server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log('Server is running on port ' + port);
 });
